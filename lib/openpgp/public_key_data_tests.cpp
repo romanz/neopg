6229beae7c433c28ba205122a603f46577a5fc6e
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-04-14 12:41:31 +0200

    Fix test.

--- lib/openpgp/public_key_data_tests.cpp
+++ lib/openpgp/public_key_data_tests.cpp
@@ -22,8 +22,9 @@ TEST(NeoPGTest, openpgp_public_key_data_test) {
         "\x00\x11\x01\x42\x23"
         "\x00\x02\x03",
         15};
-    const std::string fpr{
-        "\xb5\xb5\xbe\xc2\x3d\x70\xea\x0e\x05\x68\x45\x64\xac\xa7\x3d\xc7"};
+    auto fpr =
+        std::vector<uint8_t>{0xb5, 0xb5, 0xbe, 0xc2, 0x3d, 0x70, 0xea, 0x0e,
+                             0x05, 0x68, 0x45, 0x64, 0xac, 0xa7, 0x3d, 0xc7};
     ParserInput in(raw.data(), raw.length());
     auto key = PublicKeyData::create_or_throw(PublicKeyVersion::V3, in);
     ASSERT_EQ(key->version(), PublicKeyVersion::V3);
@@ -34,7 +35,7 @@ TEST(NeoPGTest, openpgp_public_key_data_test) {
     ASSERT_EQ(v3key->m_algorithm, PublicKeyAlgorithm::Rsa);
     ASSERT_NE(v3key->m_key, nullptr);
     ASSERT_EQ(v3key->m_key->algorithm(), PublicKeyAlgorithm::Rsa);
-    ASSERT_EQ(reinterpret_cast<char*>(v3key->fingerprint().data()), fpr);
+    ASSERT_EQ(v3key->fingerprint(), fpr);
     auto rsa = dynamic_cast<RsaPublicKeyMaterial*>(v3key->m_key.get());
     ASSERT_EQ(rsa->m_n, MultiprecisionInteger(0x14223));
     ASSERT_EQ(rsa->m_e, MultiprecisionInteger(0x3));
